0d7958088ee3a6b6af21cdc51ea691d05488682e
mateo-cogeanu <72649401+mateo-cogeanu@users.noreply.github.com>
2022-03-10 19:00:03 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Just a test file, all can be commented
+// Just a test file, all can be commented
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
