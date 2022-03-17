d363b17afd6d2d36ee301e49d95686fa6a5767ba
Munzar Ahmad <munzarahmad@gmail.com>
2022-03-17 13:26:46 +0500

    Minor

--- src/components/section1/section1.js
+++ src/components/section1/section1.js
@@ -18,12 +18,12 @@ const Section1 = () => {
     }, 500);
   }
   countLocked();
-////count for Total Trading Volume
+  ////count for Total Trading Volum
 
   function countVolume() {
     setTimeout(function () {
       var c = Math.round(Math.random() * (1000000 + 52051) + 500000000);
-      
+
       document.getElementById("random_count").innerHTML = c;
       countVolume();
     }, 500);
@@ -56,16 +56,19 @@ const Section1 = () => {
                 sx={{ display: "flex", justifyContent: "center" }}
                 // Launch App button
               >
-                <Button className={classes.buttonStyle}>   
+                <Button className={classes.buttonStyle}>
                   Launch app
                   <img
                     src={secicon}
                     style={{ marginLeft: "0.2rem", color: "#665d11" }}
                   />
                 </Button>
-                <Button className={classes.secButton}
-                // Read Documentation
-                >Read docs</Button>
+                <Button
+                  className={classes.secButton}
+                  // Read Documentation
+                >
+                  Read docs
+                </Button>
               </Grid>
             </Grid>
 
@@ -94,7 +97,6 @@ const Section1 = () => {
                 <Card sx={{ maxWidth: 300 }} className={classes.secCard}>
                   <CardActionArea>
                     <CardContent>
-                    
                       <Typography
                         gutterBottom
                         variant="h5"
@@ -106,7 +108,6 @@ const Section1 = () => {
                       <Typography
                         variant="body2"
                         className={classes.cardSubText}
-                       
                       >
                         $
                         <Typography
